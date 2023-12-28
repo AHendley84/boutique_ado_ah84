2d29a0d661d666a6872c475531ea26d121a14644
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2023-12-28 17:29:40 +0000

    Add checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,15 +7,14 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'coountry',
+                  'town_or_city', 'postcode', 'country',
                   'county',)
-        
+
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
-
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
