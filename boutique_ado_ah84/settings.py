4c81b9b4fce39d23f1404caf70b6920d739b5570
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 07:49:55 +0000

    Add django environ

--- boutique_ado_ah84/settings.py
+++ boutique_ado_ah84/settings.py
@@ -12,12 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import dj_database_url
+import environ
 
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in env
 
 ALLOWED_HOSTS = ['boutique-ado-ah84-bc688be409f8.herokuapp.com', '127.0.0.1']
 
@@ -125,7 +124,7 @@ WSGI_APPLICATION = 'boutique_ado_ah84.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in env:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
