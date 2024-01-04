9ca631c5bb0b3130a73a481e73cc91e57ebdfb2c
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 09:15:20 +0000

    Remove dotenv and used environ

--- boutique_ado_ah84/settings.py
+++ boutique_ado_ah84/settings.py
@@ -10,15 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import environ
 import dj_database_url
-if 'DEVELOPMENT' in os.environ:
-    from dotenv import load_dotenv
-    # Load environment variables from .env file
-    load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
