194be86c1640d4c73a17496680ba7b500658f516
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 07:58:39 +0000

    Disabled dotenv again to determine if it is the fault

--- boutique_ado_ah84/settings.py
+++ boutique_ado_ah84/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
-import dj_database_url
+#import dj_database_url
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
