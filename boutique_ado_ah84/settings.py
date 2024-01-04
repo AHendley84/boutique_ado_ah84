718435074dbb730f1ad2ac8b7ff375291f527bff
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 09:55:22 +0000

    Add if statement to try and resolve issue

--- boutique_ado_ah84/settings.py
+++ boutique_ado_ah84/settings.py
@@ -10,14 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-import environ
 import dj_database_url
+if os.path.isfile('.env'):
+    import environ
+    # Take environment variables from .env file
+    environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
