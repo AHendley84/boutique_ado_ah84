a53c28f41e0ffec43a29c2e47637af6c26db08b7
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 11:52:14 +0000

    Fix spelling mistake in custom_storgaes

--- custom_storages.py
+++ custom_storages.py
@@ -3,8 +3,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LCOATION
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LCOATION
+    location = settings.MEDIAFILES_LOCATION
