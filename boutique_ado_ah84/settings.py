4f8a3d4c549c7f6912df1776d54f40399bcf5e5e
Andrew Hendley <89160047+AHendley84@users.noreply.github.com>
2024-01-04 08:26:19 +0000

    Enabled dotenv

--- boutique_ado_ah84/settings.py
+++ boutique_ado_ah84/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 import dj_database_url
 import os
 
 # Load environment variables from .env file
-#load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.getenv('DEVELOPMENT')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-ah84-bc688be409f8.herokuapp.com', '127.0.0.1']
 
